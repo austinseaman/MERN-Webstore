62f07df4e5721550d062de5d95f300be2d621766
austinseaman <apseaman0@gmail.com>
2020-04-13 18:14:56 -0600

    cleaned up Home component

--- client/src/Home.js
+++ client/src/Home.js
@@ -6,30 +6,8 @@ import './App.css'
 
 function Home() {
     return (
-        <div className='nav-container'>
-            {/* <span className="socials">
-                <a href="https://www.instagram.com/vitamnk/">
-                    <img src={Insta} alt="instagram-logo"/>
-                </a>
-                <a href="https://www.facebook.com/kaya.poore">
-                    <img src={Facebook} alt="facebook-logo"/>
-                </a>
-            </span> */}
-            <span className='nav-logo'>
-                <Link to='/'>
-                    <h1>Kaya Poore</h1>
-                </Link>
-            </span>
-            <div className='nav-right'>
-                <span className='dropdown'>
-                    <h3 className='dropdown-title'>Menu</h3>
-                    <div className='dropdown-content'>
-                        <Link to="/gallery"><h2>Gallery</h2></Link>
-                        <Link to="/contact"><h2>Contact</h2></Link>
-                        <Link to="/cart"><h2>Cart</h2></Link>
-                    </div>
-                </span>
-            </div>
+        <div>
+            
         </div>
     )
 }
